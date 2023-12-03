a720a0654b829ba8cf9b9b1cef6d97af0b9c53c4
nobregaX <otavio.nobregadas@gmail.com>
2023-12-03 23:47:36 +0000

    products: dubai: update dependencies
    Change-Id: I561fb452cc8ae0d075cbe22b62dd15397a61b335

--- products/dubai/aospa_dubai.mk
+++ products/dubai/aospa_dubai.mk
@@ -8,7 +8,7 @@ ifeq (aospa_dubai,$(TARGET_PRODUCT))
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from the custom device configuration.
 $(call inherit-product, device/motorola/dubai/device.mk)
